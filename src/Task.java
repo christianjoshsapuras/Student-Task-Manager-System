611e4b3aeb4b2275d9999f71db853fd530a60d1e
Christian Josh Sapuras <155878865+christianjoshsapuras@users.noreply.github.com>
2026-04-25 16:58:02 +0800

    Added Task class and attributes

---
+++ src/Task.java
@@ -0,0 +1,12 @@
+package taskmanager;
+import java.util.Date;
+
+public class Task {
+	
+	private int taskID;
+	private String title;
+	private String description;
+	private Date dueDate;
+	private boolean status;
+		
+}
